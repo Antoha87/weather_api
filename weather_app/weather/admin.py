56b12850cf0e4dbceb9bf5654be5b7e5ec09f75e
dimcheck <dfarinyak@gmail.com>
2021-07-13 12:28:48 +0300

    add admin

--- weather_app/weather/admin.py
+++ weather_app/weather/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Weather
+
+
+class WeatherAdmin(admin.ModelAdmin):
+    list_filter = ('location', 'loc_time', 'temperature')
+    search_fields = ('location', 'temperature')
+    list_display = ('location', 'loc_time', 'temperature')
+
+admin.site.register(Weather, WeatherAdmin)
 
 # Register your models here.
