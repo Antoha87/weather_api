96ec31309d45fa14f4bd5e9af77b85498380a649
Bohdan <bogdan.loljoj@gmail.com>
2021-07-22 17:56:36 +0300

    Implemented frontend part with React

--- frontend/front_app/src/App.js
+++ frontend/front_app/src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class MyComponent extends Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        error: null,
+        goods: []
+      }
+    }
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/api/goods/?format=json")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            goods: result
+          });
+        },
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
+  }
+
+  render() {
+    const { error, goods } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else {
+      return (
+          <div class="col-8 offset-2">
+            <h1 class="d-inline">Goods list</h1><img class="w-25 h-25 d-inline" src="https://www.pngitem.com/pimgs/m/533-5331872_black-business-people-png-transparent-png.png"/>
+            <table>
+                <tr class="border text-light bg-dark">
+                    <th class="col-3">ID</th><th class="col-3">NAME</th><th class="col-3">PRICE</th>
+                </tr>
+                  {goods.map(good => (
+                  <tr class="border" key={good.id}>
+                       <td class="col-3">{good.id}</td><td class="col-3 bg-primary text-white">{good.name}</td><td class="col-3 text-success bg-warning">£{good.price}</td>
+                  </tr>
+                  ))}
+            </table>
+          </div>
+      );
+    }
+  }
 }
 
-export default App;
+export default MyComponent;
